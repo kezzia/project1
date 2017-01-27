0513c643fc083990a7e0b99ea2cbb67416886370
undeadVivisector <mvandertramps@gmail.com>
2017-01-27 05:00:24 -0500

    fixed question 1's unhelpful comments

--- fp.rkt
+++ fp.rkt
@@ -3,11 +3,11 @@
 ;QUESTION 1: Given a list (sometimes nested), this function reverses the order of the list
 (define (reverse-general L)
   (cond
-    ((null? L)L) ;if the list is empty it just returns the empty list
-    ((list? L) ;if the list is not empty, pop off the first element until only one remains in the list, append the first elements one by one  
+    ((null? L)L) ;if the list is empty return the empty list
+    ((list? L) ;if the list is not empty, take the first element and append it to the list recursively
       (append (reverse-general (cdr L))(list (reverse-general (car L)))
     ))
-    (else L) ;return the list
+    (else L) ;returns the call stack
   )
 )
 
